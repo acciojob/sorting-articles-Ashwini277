16be1a06f3ed024d78ecf3a0d987b8ae78c20ede
Ashwini277 <devarushiashwini@gmail.com>
2024-08-30 09:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const bands = [
+        'The Plot in You', 
+        'The Devil Wears Prada', 
+        'Pierce the Veil', 
+        'Norma Jean', 
+        'The Bled', 
+        'Say Anything', 
+        'The Midway State', 
+        'We Came as Romans', 
+        'Counterparts', 
+        'Oh, Sleeper', 
+        'A Skylit Drive', 
+        'Anywhere But Here', 
+        'An Old Dog'
+    ];
+
+    const removeArticles = str => str.replace(/\b(a|an|the)\b\s*/gi, '');
+
+    const sortedBands = bands
+        .map(removeArticles)
+        .sort()
+        .map(band => `<li>${band}</li>`)
+        .join('');
+
+    document.getElementById('bands').innerHTML = sortedBands;
+});
+
